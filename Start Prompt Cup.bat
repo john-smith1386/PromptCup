11af9d635c7f34cf6d48a81681d0628eb282474e
john-smith1386 <96865828+john-smith1386@users.noreply.github.com>
2026-09-18 01:31:08 -0700

    udpated to buymeacoffee

--- Start Prompt Cup.bat
+++ Start Prompt Cup.bat
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 @echo off
 title Prompt Cup
 cd /d "%~dp0"
 node bin\cli.js start
 pause
+=======
+@echo off
+title Prompt Cup
+cd /d "%~dp0"
+node bin\cli.js start
+pause
+>>>>>>> 3aa2c57 (udpated to buymeacoffee)
